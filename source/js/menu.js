59a5c61b0a1dd2cccc60c33061c29659a303b5b2
sne <snezhana.syngur@gmail.com>
2019-12-07 12:01:33 +0100

    Change points with sizes

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,14 +1,14 @@
 'use strict';
 
 (function () {
-  var buttonMenu = document.querySelector('.main-nav__toggle');
-  var nav = document.querySelector('.main-nav--closed');
+  var buttonMenu = document.querySelector('.nav__toggle');
+  var nav = document.querySelector('.nav--closed');
   var menu = document.querySelector('.menu--closed');
 
 
   buttonMenu.addEventListener('click', function (evt) {
     evt.preventDefault();
-    nav.classList.toggle('main-nav--opened');
+    nav.classList.toggle('nav--opened');
     menu.classList.toggle('menu--opened');
   });
 })();
