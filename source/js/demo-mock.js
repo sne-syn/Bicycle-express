f1fd344e6433881ca6bb2ff2d963a95f8e58ca36
sne <snezhana.syngur@gmail.com>
2019-12-17 13:37:57 +0100

    Render product cards to the demo page

--- source/js/demo-mock.js
+++ source/js/demo-mock.js
@@ -2,51 +2,48 @@
 
 (function () {
 
-var PHOTOS = ['../img/product-demo-1.png', '../img/product-demo-2.png', '../img/product-demo-3.png'];
-var BRAND_NAMES = ['Felt', 'Raleigh', 'Raleigh'];
-var MODELS = ['Decree FRD', 'Port Townsend', 'Redux 2'];
-var PRICES = ['8.999.00', '759.99', '679.99'];
+  var PHOTOS = ['../img/product-demo-1.png', '../img/product-demo-2.png', '../img/product-demo-3.png', '../img/product-demo-4.png', '../img/product-demo-5.png', '../img/product-demo-6.png', '../img/product-demo-7.png', '../img/product-demo-8.png', '../img/product-demo-9.png'];
+  var BRAND_NAMES = ['Felt', 'Raleigh', 'Raleigh', 'Raleigh', 'Felt', 'Raleigh', 'Unknown', 'Unknown', 'Felt'];
+  var MODELS = ['Decree FRD', 'Port Townsend', 'Redux 2', 'Gala', 'Vezza Cruz', 'Tamland - 2', 'Bass-3 Complete', 'Type - 1', 'Decree 5'];
+  var PRICES = [8999.00, 759.99, 679.99, 449.99, 499.00, 2399.99, 599.99, 699.99, 3499.00];
 
-var cardQuantity = 3;
-var cards = [];
+  var cardQuantity = 9;
+  var cards = [];
 
-var generateProductCard = function (cardQuantity, array) {
-  for (var i = 0; i < cardQuantity; i++) {
-    array.push({
-      'photo': PHOTOS[i],
-      'brand': BRAND_NAMES[i],
-      'model': MODELS[i],
-      'price': PRICES[i]
-    });
-  }
-};
-
-generateProductCard(cardQuantity, cards);
-
-var productBlock = document.querySelector('.product');
-var productTemplate = document.querySelector('#demo-product').content
-.querySelector('.product__item');
-
-  var renderProductCard = function (obj) {
-    var productCard = productTemplate.cloneNode(true);
-    productBlock.insertBefore(productCard, null);
-
-    var image = productCard.querySelector('.product__img');
-    image.src = obj.photo;
-    image.width = 282;
-    image.height = 178;
-
-    var brand = productCard.querySelector('.product__brand');
-    brand.textContent = obj.brand;
-
-    var model = productCard.querySelector('.product__name');
-    model.textContent = obj.model;
-
-    var price = productCard.querySelector('.product__price');
-    price.textContent = obj.price;
-
+  var createProductCard = function () {
+    for (var i = 0; i < cardQuantity; i++) {
+      cards.push({
+        'photo': PHOTOS[i],
+        'brand': BRAND_NAMES[i],
+        'model': MODELS[i],
+        'price': '$ ' + PRICES[i]
+      });
+    }
   };
 
-  renderProductCard(cards[1]);
+  createProductCard();
+
+  var renderProductCard = function (cards) {
+    var productBlock = document.querySelector('.product');
+    productBlock.innerHTML = '';
+
+    cards.forEach(function (item) {
+      var productTemplate = document.querySelector('#demo-product').content.querySelector('.product__item');
+      var productCard = productTemplate.cloneNode(true);
+      productBlock.insertBefore(productCard, null);
+      var image = productCard.querySelector('.product__img');
+      image.src = item.photo;
+
+      var brand = productCard.querySelector('.product__brand');
+      brand.textContent = item.brand;
+
+      var model = productCard.querySelector('.product__name');
+      model.textContent = item.model;
+
+      var price = productCard.querySelector('.product__price');
+      price.textContent = item.price;
+    });
+  };
+  renderProductCard(cards);
 
 })();
